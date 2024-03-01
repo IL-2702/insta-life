b8229d3952db1f12e46e340190d28c3ac1c4ada0
zanzipanziby <zanzipanziby@gmail.com>
2024-03-01 16:36:44 +0300

    feat: add base layout to all stab pages

--- src/pages/auth/signUp.tsx
+++ src/pages/auth/signUp.tsx
@@ -1,5 +1,8 @@
+import { getBaseLayout } from '@/layouts/publ/BaseLayout/BaseLayout'
+
 const SignUp = () => {
   return <div>SignUp</div>
 }
 
 export default SignUp
+SignUp.getLayout = getBaseLayout
