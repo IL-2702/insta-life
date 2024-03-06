f5a5903f79e3975490ea08a3eafa4aba3a63e0a4
ZanziPanziBY <zanzipanziby@gmail.com>
2024-03-06 12:53:48 +0300

    IL-22 fix: incorrect import eye icon

--- src/shared/ui/Textfield/TextField.tsx
+++ src/shared/ui/Textfield/TextField.tsx
@@ -1,7 +1,6 @@
 import React, { ComponentPropsWithoutRef, FC, KeyboardEvent, ReactNode, useState } from 'react'
 
 import { Close } from '@/shared/assets/icons/Close'
-import { Eye } from '@/shared/assets/icons/Eye'
 import { Search } from '@/shared/assets/icons/SearchOutline'
 import { VisibilityOff } from '@/shared/assets/icons/VisibilityOff'
 import { Typography } from '@/shared/ui/Typography'
@@ -9,6 +8,8 @@ import * as Label from '@radix-ui/react-label'
 import clsx from 'clsx'
 
 import s from './TextField.module.scss'
+
+import { Eye } from '../../assets/icons/Eye'
 
 export type TextFieldProps = {
   className?: string
