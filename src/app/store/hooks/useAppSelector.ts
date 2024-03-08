1e994d82c13d251add84c7073b6b8d0fc5951740
Svyatoslav Nemkovich <svyat5555@gmail.com>
2024-03-08 16:26:10 +0300

    IL-31 feat: add useAppSelector hook

---
+++ src/app/store/hooks/useAppSelector.ts
@@ -0,0 +1,5 @@
+import { TypedUseSelectorHook, useSelector } from 'react-redux'
+
+import { RootState } from '../types/rootState'
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
