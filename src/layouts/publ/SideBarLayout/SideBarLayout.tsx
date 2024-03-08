832c8b6fecc62a58b97688a0817c9be5fea40791
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-03-08 16:52:38 +0300

    IL-28 feat: add SideBar in src/layouts/local and use it in MainLayout

--- src/layouts/publ/SideBarLayout/SideBarLayout.tsx
+++ src/layouts/publ/SideBarLayout/SideBarLayout.tsx
@@ -1,15 +1,15 @@
 import { PropsWithChildren } from 'react'
 
-import SideBar from '@/layouts/local/ui/SideBar'
+import { SideBar } from '@/layouts/local/ui/SideBar'
 import { NextPage } from 'next'
 
-export const MainLayout: NextPage<PropsWithChildren> = props => {
+export const SideBarLayout: NextPage<PropsWithChildren> = props => {
   const { children } = props
 
   return (
-    <div>
+    <>
       <SideBar />
       <div>{children}</div>
-    </div>
+    </>
   )
 }
---
+++ src/pages/create/index.tsx
@@ -0,0 +1,8 @@
+import { getBaseLayout } from '@/layouts/publ/BaseLayout/BaseLayout'
+
+const CreatePage = () => {
+  return <div>Create</div>
+}
+
+CreatePage.getLayout = getBaseLayout
+export default CreatePage
