570197b73e5448101ad2da7fead20334d2909a30
Mannomann <krivmann00@gmail.com>
2024-03-10 14:33:43 +0200

    IL-16: feat add auth endpoint

--- src/services/authService/authEndpoints.ts
+++ src/services/authService/authEndpoints.ts
@@ -1,5 +1,30 @@
 import { api } from '@/services/api'
+import { SignInParams, SignInResponse } from '@/services/authService/lib/authEndpoints.types'
+import { authActions } from '@/services/authService/store/slice/authEndpoints.slice'
 
 const authEndpoints = api.injectEndpoints({
-  endpoints: builder => ({}),
+  endpoints: builder => ({
+    signIn: builder.mutation<SignInResponse, SignInParams>({
+      onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
+        try {
+          const {
+            data: { accessToken },
+          } = await queryFulfilled
+
+          if (accessToken) {
+            dispatch(authActions.setAccessToken(accessToken))
+          }
+        } catch (e) {
+          console.log(e)
+        }
+      },
+      query: args => ({
+        body: args,
+        method: 'POST',
+        url: 'auth/login',
+      }),
+    }),
+  }),
 })
+
+export const { useSignInMutation } = authEndpoints
