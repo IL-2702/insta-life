a06a7e46b46ecbc722114d834c422d71a7d42b05
Mannomann <krivmann00@gmail.com>
2024-03-11 21:17:04 +0200

    IL-30.1: feat add ROUTES constant

--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -1,11 +1,23 @@
-export const PUBLIC_ROUTES = ['/sign-in', '/sign-up', '/forgot-password']
+export const ROUTES = {
+  CREATE: '/create',
+  DEFAULT: '/',
+  FAVORITES: '/favorites',
+  FORGOT_PASS: '/forgot-password',
+  HOME: '/home',
+  LOGIN: '/sign-in',
+  MESSENGER: '/messenger',
+  PROFILE: '/profile',
+  REGISTER: '/sign-up',
+  SEARCH: '/search',
+  STATISTICS: '/statistics',
+}
 export const PRIVATE_ROUTES = [
-  '/',
-  '/home',
-  '/create',
-  '/profile',
-  '/messenger',
-  '/search',
-  '/statistics',
-  '/favorites',
+  ROUTES.DEFAULT,
+  ROUTES.HOME,
+  ROUTES.CREATE,
+  ROUTES.PROFILE,
+  ROUTES.SEARCH,
+  ROUTES.STATISTICS,
+  ROUTES.FAVORITES,
+  ROUTES.MESSENGER,
 ]
