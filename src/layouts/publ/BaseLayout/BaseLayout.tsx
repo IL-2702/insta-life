ac15c4ec7dd63ac4d6eb276a47c65fe3318d9ad3
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-03-11 13:26:11 +0300

    IL-33 feat: split layouts

--- src/layouts/publ/BaseLayout/BaseLayout.tsx
+++ src/layouts/publ/BaseLayout/BaseLayout.tsx
@@ -1,12 +1,28 @@
 import { PropsWithChildren, ReactElement } from 'react'
 
-import { SideBarLayout } from '@/layouts/publ/SideBarLayout/SideBarLayout'
+import { AuthLayout } from '@/layouts/publ/AuthLayout'
+import { MainLayout } from '@/layouts/publ/MainLayout'
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
 const BaseLayout: NextPage<PropsWithChildren> = props => {
   const { children } = props
 
-  return <SideBarLayout>{children}</SideBarLayout>
+  const router = useRouter()
+  const { pathname } = router
+
+  const isSignInPathname = pathname === '/auth/sign-in'
+  const isSignUpPathname = pathname === '/auth/sign-up'
+
+  return (
+    <>
+      {isSignInPathname || isSignUpPathname ? (
+        <AuthLayout>{children}</AuthLayout>
+      ) : (
+        <MainLayout>{children}</MainLayout>
+      )}
+    </>
+  )
 }
 
 export const getBaseLayout = (page: ReactElement) => {
