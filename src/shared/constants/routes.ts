dd497c6e57e2c94db50d260544398db04a3a9ed0
Mannomann <krivmann00@gmail.com>
2024-03-11 14:06:07 +0200

    IL-30: feat add private routes

---
+++ src/shared/constants/routes.ts
@@ -0,0 +1,11 @@
+export const PUBLIC_ROUTES = ['/sign-in', '/sign-up', '/forgot-password']
+export const PRIVATE_ROUTES = [
+  '/',
+  '/home',
+  '/create',
+  '/profile',
+  '/messenger',
+  '/search',
+  '/statistics',
+  '/favorites',
+]
