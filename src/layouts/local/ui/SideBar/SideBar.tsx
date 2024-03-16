0fec50690e21cba8ea793e28a733b840d6c36ed6
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-03-17 00:38:35 +0300

    IL-44 refactor: change paths to variables in Header and SideBar

--- src/layouts/local/ui/SideBar/SideBar.tsx
+++ src/layouts/local/ui/SideBar/SideBar.tsx
@@ -6,6 +6,7 @@ import { MessengerIcon } from '@/shared/assets/icons/asideIcons/messengerIcon'
 import { ProfileIcon } from '@/shared/assets/icons/asideIcons/profileIcon'
 import { SearchIcon } from '@/shared/assets/icons/asideIcons/searchIcon'
 import { StatisticsIcon } from '@/shared/assets/icons/asideIcons/statisticsIcon'
+import { ROUTES } from '@/shared/constants/routes'
 import { Typography } from '@/shared/ui/Typography'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -18,49 +19,49 @@ export const SideBar = () => {
 
   return (
     <aside className={s.aside}>
-      <Link className={pathname === '/home' ? s.activeLink : ''} href={'/home'}>
+      <Link className={pathname === ROUTES.HOME ? s.activeLink : ''} href={ROUTES.HOME}>
         <HomeIcon className={s.navIcon} />
         <Typography className={s.navLink} color={'light'} variant={'medium14'}>
           Home
         </Typography>
       </Link>
-      <Link className={pathname === '/create' ? s.activeLink : ''} href={'/create'}>
+      <Link className={pathname === ROUTES.CREATE ? s.activeLink : ''} href={ROUTES.CREATE}>
         <CreateIcon className={s.navIcon} />
         <Typography className={s.navLink} color={'light'} variant={'medium14'}>
           Create
         </Typography>
       </Link>
-      <Link className={pathname === '/profile' ? s.activeLink : ''} href={'/profile'}>
+      <Link className={pathname === ROUTES.PROFILE ? s.activeLink : ''} href={ROUTES.PROFILE}>
         <ProfileIcon className={s.navIcon} />
         <Typography className={s.navLink} color={'light'} variant={'medium14'}>
           MyProfile
         </Typography>
       </Link>
-      <Link className={pathname === '/messenger' ? s.activeLink : ''} href={'/messenger'}>
+      <Link className={pathname === ROUTES.MESSENGER ? s.activeLink : ''} href={ROUTES.MESSENGER}>
         <MessengerIcon className={s.navIcon} />
         <Typography className={s.navLink} color={'light'} variant={'medium14'}>
           Messenger
         </Typography>
       </Link>
-      <Link className={pathname === '/search' ? s.activeLink : ''} href={'/search'}>
+      <Link className={pathname === ROUTES.SEARCH ? s.activeLink : ''} href={ROUTES.SEARCH}>
         <SearchIcon className={s.navIcon} />
         <Typography className={s.navLink} color={'light'} variant={'medium14'}>
           Search
         </Typography>
       </Link>
-      <Link className={pathname === '/statistics' ? s.activeLink : ''} href={'/statistics'}>
+      <Link className={pathname === ROUTES.STATISTICS ? s.activeLink : ''} href={ROUTES.STATISTICS}>
         <StatisticsIcon className={s.navIcon} />
         <Typography className={s.navLink} color={'light'} variant={'medium14'}>
           Statistics
         </Typography>
       </Link>
-      <Link className={pathname === '/favorites' ? s.activeLink : ''} href={'/favorites'}>
+      <Link className={pathname === ROUTES.FAVORITES ? s.activeLink : ''} href={ROUTES.FAVORITES}>
         <BookMark className={s.navIcon} />
         <Typography className={s.navLink} color={'light'} variant={'medium14'}>
           Favorites
         </Typography>
       </Link>
-      <Link href={'/auth/sign-in'}>
+      <Link href={ROUTES.LOGIN}>
         <LogOutIcon className={s.navIcon} />
         <Typography className={s.navLink} color={'light'} variant={'medium14'}>
           Log Out
