ac7819130ed49aef1a00321a7bd2f385282f77b6
Svyatoslav Nemkovich <svyat5555@gmail.com>
2024-03-18 18:01:55 +0300

    IL-15 feat: add ui for RegistrationConfirmation widget

---
+++ src/widgets/auth/registrationConfirmation/ui/RegistrationConfirmation.tsx
@@ -0,0 +1,7 @@
+import { memo } from 'react'
+
+import { RegistrationConfirmationProps } from '@/widgets/auth/registrationConfirmation/container'
+
+export const RegistrationConfirmation = memo(({}: RegistrationConfirmationProps) => {
+  return <></>
+})
