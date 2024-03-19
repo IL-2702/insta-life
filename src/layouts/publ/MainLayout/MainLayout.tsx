3a1c48f73955212c998ed15a285539db3f83a90b
Mannomann <krivmann00@gmail.com>
2024-03-19 23:30:17 +0200

    IL-FIX: rollback authProvider

--- src/layouts/publ/MainLayout/MainLayout.tsx
+++ src/layouts/publ/MainLayout/MainLayout.tsx
@@ -1,27 +1,11 @@
-import { PropsWithChildren, useEffect } from 'react'
+import { PropsWithChildren } from 'react'
 
-import { useAppSelector } from '@/app/store/hooks/useAppSelector'
 import { Header } from '@/layouts/local/ui/Header'
 import { SideBar } from '@/layouts/local/ui/SideBar'
-import { ROUTES } from '@/shared/constants/routes'
 import { NextPage } from 'next'
-import { useRouter } from 'next/router'
 
 export const MainLayout: NextPage<PropsWithChildren> = props => {
   const { children } = props
-  const { push } = useRouter()
-
-  const token = useAppSelector(state => state.authReducer.accessToken)
-
-  useEffect(() => {
-    if (!token) {
-      void push(ROUTES.LOGIN)
-    }
-  }, [token])
-
-  if (!token) {
-    return null
-  }
 
   return (
     <>
--- src/pages/github/index.tsx
+++ src/pages/github/index.tsx
@@ -1,19 +1,15 @@
 import { useEffect } from 'react'
 
-import { useAppDispatch } from '@/app/store/hooks/useAppDispatch'
 import { getBaseLayout } from '@/layouts/publ/BaseLayout/BaseLayout'
-import { authActions } from '@/services/authService/store/slice/authEndpoints.slice'
 import { ROUTES } from '@/shared/constants/routes'
 import { Spinner } from '@/shared/ui/Spinner'
 import { useRouter } from 'next/router'
 
 const GitHubPage = () => {
   const { push, query } = useRouter()
-  const dispatch = useAppDispatch()
 
   useEffect(() => {
     if (query.accessToken) {
-      dispatch(authActions.setAccessToken(query.accessToken as string))
       push(ROUTES.HOME)
     } else {
       push(ROUTES.LOGIN)
