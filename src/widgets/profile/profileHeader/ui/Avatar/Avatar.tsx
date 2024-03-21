77132eeee768a711fb1b9ea0df71f4bf78275ad6
zanzipanziby <zanzipanziby@gmail.com>
2024-03-22 01:16:21 +0300

    IL-52 feat: add Avatar component and types to profile header

--- src/widgets/profile/profileHeader/ui/Avatar/Avatar.tsx
+++ src/widgets/profile/profileHeader/ui/Avatar/Avatar.tsx
@@ -1,13 +1,23 @@
 import { Typography } from '@/shared/ui/Typography'
+import { Avatar as AvatarType } from '@/widgets/profile/types'
+import Image from 'next/image'
 
 import s from './Avatar.module.scss'
 
-export const Avatar = () => {
-  return (
+export const Avatar = ({ avatar }: Props) => {
+  const avatarRender = avatar ? (
+    <Image alt={'avatar'} height={avatar.height} src={avatar.url} width={avatar.width} />
+  ) : (
     <div className={s.avatar}>
       <Typography as={'span'} variant={'h1'}>
         Avatar
       </Typography>
     </div>
   )
+
+  return <>{avatarRender}</>
+}
+
+type Props = {
+  avatar: AvatarType
 }
--- src/widgets/profile/profileHeader/ui/ProfileHeader.tsx
+++ src/widgets/profile/profileHeader/ui/ProfileHeader.tsx
@@ -1,10 +1,10 @@
 import { ProfileHeaderProps } from '@/widgets/profile/profileHeader/container'
 import { Avatar } from '@/widgets/profile/profileHeader/ui/Avatar/Avatar'
 
-export const ProfileHeader = ({}: ProfileHeaderProps) => {
+export const ProfileHeader = ({ avatar }: ProfileHeaderProps | any) => {
   return (
     <div>
-      <Avatar />
+      <Avatar avatar={avatar} />
       Profile Info
     </div>
   )
