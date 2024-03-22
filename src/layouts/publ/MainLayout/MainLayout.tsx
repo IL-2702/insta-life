05682c80e84bbb72492af62318130734929a00ef
zanzipanziby <zanzipanziby@gmail.com>
2024-03-23 01:41:00 +0300

    IL-55 fix: change style for main layout and child components

--- src/layouts/publ/MainLayout/MainLayout.tsx
+++ src/layouts/publ/MainLayout/MainLayout.tsx
@@ -3,7 +3,10 @@ import { PropsWithChildren } from 'react'
 import { AuthProvider } from '@/app/providers/authProvider'
 import { Header } from '@/layouts/local/ui/Header'
 import { SideBar } from '@/layouts/local/ui/SideBar'
+import { Container } from '@/shared/ui/Container'
 import { NextPage } from 'next'
+
+import s from './MainLayout.module.scss'
 
 export const MainLayout: NextPage<PropsWithChildren> = props => {
   const { children } = props
@@ -11,8 +14,10 @@ export const MainLayout: NextPage<PropsWithChildren> = props => {
   return (
     <AuthProvider>
       <Header />
-      <SideBar.widget />
-      <div>{children}</div>
+      <Container className={s.wrapper}>
+        <SideBar.widget />
+        <main className={s.main}>{children}</main>
+      </Container>
     </AuthProvider>
   )
 }
