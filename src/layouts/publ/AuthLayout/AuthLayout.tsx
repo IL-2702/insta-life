a80c92a4809a0237581510a8d10c5c26186e611c
zanzipanziby <zanzipanziby@gmail.com>
2024-03-26 02:54:18 +0300

    IL-55 fix: fix uncorrected style in main-layout

--- src/layouts/publ/AuthLayout/AuthLayout.tsx
+++ src/layouts/publ/AuthLayout/AuthLayout.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren, useEffect } from 'react'
 
 import { Header } from '@/layouts/local/ui/Header'
 import { Container } from '@/shared/ui/Container'
+import { Wrapper } from '@/shared/ui/Wrapper'
 import { NextPage } from 'next'
 
 import s from './AuthLayout.module.scss'
@@ -9,11 +10,11 @@ export const AuthLayout: NextPage<PropsWithChildren> = props => {
   const { children } = props
 
   return (
-    <>
+    <Wrapper>
       <Header />
       <main className={s.main}>
         <Container className={s.container}>{children}</Container>
       </main>
-    </>
+    </Wrapper>
   )
 }
