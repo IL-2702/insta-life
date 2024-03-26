97a1c84cce6dcb23ddfaa00f2119ac7dbfaad265
Papperla <krivman@Savar.local>
2024-03-26 14:31:53 +0200

    IL-53 feat: add deleteAva loading status

--- src/widgets/profile/uploadUserPhoto/ui/form/UploadUserPhotoForm.tsx
+++ src/widgets/profile/uploadUserPhoto/ui/form/UploadUserPhotoForm.tsx
@@ -9,36 +9,33 @@ import 'react-image-crop/src/ReactCrop.scss'
 import s from './UploadUserPhotoForm.module.scss'
 
 export const UploadUserPhotoForm = ({ currUserPhoto, onClose }: Props) => {
-  const {
-    control,
-    extraActionsUserPhoto,
-    handleSubmit,
-    isLoadingUploadAvatar,
-    uploadAvatarHandler,
-    userPhoto,
-    userPhotoError,
-  } = useUploadUserPhotoForm(currUserPhoto, onClose)
-  const classNames = {
-    errorWrapper: clsx(s.errorWrapper, !userPhotoError && s.hidden),
-    form: clsx(s.form, !userPhotoError && s.noError),
-  }
+  // const {
+  //   control,
+  //   extraActionsUserPhoto,
+  //   handleSubmit,
+  //   isLoadingUploadAvatar,
+  //   uploadAvatarHandler,
+  //   userPhoto,
+  //   userPhotoError,
+  // } = useUploadUserPhotoForm(currUserPhoto, onClose)
+
+  // const classNames = {
+  //   errorWrapper: clsx(s.errorWrapper, !userPhotoError && s.hidden),
+  //   form: clsx(s.form, !userPhotoError && s.noError),
+  // }
 
   return (
     <>
-      <div className={classNames.errorWrapper}>
-        {userPhotoError && <Typography variant={'error'}>{userPhotoError}</Typography>}
+      <div className={s.errorWrapper}>
+        {/*{userPhotoError && <Typography variant={'error'}>{userPhotoError}</Typography>}*/}
       </div>
 
-      <form className={classNames.form} onSubmit={handleSubmit(data => {})}>
-        {userPhoto ? (
-          <UserPhotoCrop
-            isLoading={isLoadingUploadAvatar}
-            uploadAvatar={uploadAvatarHandler}
-            userPhoto={userPhoto}
-          />
+      <form onSubmit={() => {}}>
+        {currUserPhoto ? (
+          <UserPhotoCrop isLoading uploadAvatar={() => {}} userPhoto={currUserPhoto} />
         ) : (
-          <div>noCover</div>
-          // <NoCover control={control} extraActionsUserPhoto={extraActionsUserPhoto} />
+          <div></div>
+          //<NoCover control={control} extraActionsUserPhoto={extraActionsUserPhoto} />
         )}
       </form>
     </>
