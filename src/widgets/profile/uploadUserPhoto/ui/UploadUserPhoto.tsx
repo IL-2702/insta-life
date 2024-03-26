530c606f63a159e0c9423631417c449286dc24f9
Papperla <krivman@Savar.local>
2024-03-26 14:07:04 +0200

    IL-53 feat: add deleteAva loading status

--- src/widgets/profile/uploadUserPhoto/ui/UploadUserPhoto.tsx
+++ src/widgets/profile/uploadUserPhoto/ui/UploadUserPhoto.tsx
@@ -27,55 +27,55 @@ export const UploadUserPhoto = memo(
   }: UploadUserPhotoProps) => {
     return (
       <>
-        {/*<div className={s.container}>*/}
-        {/*  <div className={s.avatarWrapper}>*/}
-        {/*    <Avatar height={avaHeight} userAvatar={avatar} width={avaWidth} />*/}
-        {/*    {avatar && (*/}
-        {/*      <Button className={s.delAvatarBtn} onClick={() => setIsDeleteOpen(true)}>*/}
-        {/*        <Image alt={'delete Avatar'} height={16} src={close} width={16} />*/}
-        {/*      </Button>*/}
-        {/*    )}*/}
-        {/*  </div>*/}
-        {/*  <Button onClick={() => setIsUploadOpen(true)} variant={'outlined'}>*/}
-        {/*    <Typography variant={'h3'}>{t.button.addAProfilePhoto}</Typography>*/}
-        {/*  </Button>*/}
-        {/*</div>*/}
-        {/*<Modal*/}
-        {/*  className={s.modal}*/}
-        {/*  customButtonsBlock={<></>}*/}
-        {/*  modalHandler={setIsUploadOpen}*/}
-        {/*  open={isUploadOpen}*/}
-        {/*  title={t.modal.addPhotoModalTitle}*/}
-        {/*>*/}
-        {/*  <UploadUserPhotoForm onClose={() => setIsUploadOpen(false)} />*/}
-        {/*</Modal>*/}
-        {/*<Modal*/}
-        {/*  className={s.modal}*/}
-        {/*  customButtonsBlock={<></>}*/}
-        {/*  modalHandler={setIsDeleteOpen}*/}
-        {/*  open={isDeleteOpen}*/}
-        {/*  title={t.modal.deleteUserAvatar}*/}
-        {/*>*/}
-        {/*  <div>*/}
-        {/*    <Typography variant={'regular16'}>{t.modal.deleteUserAvatarText}</Typography>*/}
-        {/*    <div className={s.confirmButtonWrapper}>*/}
-        {/*      <Button*/}
-        {/*        disabled={isLoadingDeleteAvatar}*/}
-        {/*        isLoading={isLoadingDeleteAvatar}*/}
-        {/*        onClick={deleteAvatarHandler}*/}
-        {/*      >*/}
-        {/*        <Typography variant={'h3'}>{t.button.yes}</Typography>*/}
-        {/*      </Button>*/}
-        {/*      <Button*/}
-        {/*        disabled={isLoadingDeleteAvatar}*/}
-        {/*        onClick={() => setIsDeleteOpen(false)}*/}
-        {/*        variant={'outlined'}*/}
-        {/*      >*/}
-        {/*        <Typography variant={'h3'}>{t.button.no}</Typography>*/}
-        {/*      </Button>*/}
-        {/*    </div>*/}
-        {/*  </div>*/}
-        {/*</Modal>*/}
+        <div className={s.container}>
+          {/*<div className={s.avatarWrapper}>*/}
+          {/*  <Avatar height={avaHeight} userAvatar={avatar} width={avaWidth} />*/}
+          {/*  {avatar && (*/}
+          {/*    <Button className={s.delAvatarBtn} onClick={() => setIsDeleteOpen(true)}>*/}
+          {/*      <Image alt={'delete Avatar'} height={16} src={close} width={16} />*/}
+          {/*    </Button>*/}
+          {/*  )}*/}
+          {/*</div>*/}
+          <Button onClick={() => setIsUploadOpen(true)} variant={'outlined'}>
+            <Typography variant={'h3'}>{t.button.addAProfilePhoto}</Typography>
+          </Button>
+        </div>
+        <Modal
+          className={s.modal}
+          customButtonsBlock={<></>}
+          modalHandler={setIsUploadOpen}
+          open={isUploadOpen}
+          title={t.modal.addPhotoModalTitle}
+        >
+          <UploadUserPhotoForm onClose={() => setIsUploadOpen(false)} />
+        </Modal>
+        <Modal
+          className={s.modal}
+          customButtonsBlock={<></>}
+          modalHandler={setIsDeleteOpen}
+          open={isDeleteOpen}
+          title={t.modal.deleteUserAvatar}
+        >
+          <div>
+            <Typography variant={'regular16'}>{t.modal.deleteUserAvatarText}</Typography>
+            <div className={s.confirmButtonWrapper}>
+              <Button
+                disabled={isLoadingDeleteAvatar}
+                isLoading={isLoadingDeleteAvatar}
+                onClick={deleteAvatarHandler}
+              >
+                <Typography variant={'h3'}>{t.button.yes}</Typography>
+              </Button>
+              <Button
+                disabled={isLoadingDeleteAvatar}
+                onClick={() => setIsDeleteOpen(false)}
+                variant={'outlined'}
+              >
+                <Typography variant={'h3'}>{t.button.no}</Typography>
+              </Button>
+            </div>
+          </div>
+        </Modal>
       </>
     )
   }
